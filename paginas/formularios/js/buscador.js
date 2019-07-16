a07898b0780d3ee5719a63aa05d5746e0fb53254
zJesusJavier <zjesusjavierxp@gmail.com>
2019-07-16 11:45:08 -0400

    Funcion de Filtro de Candidatas

--- paginas/formularios/js/buscador.js
+++ paginas/formularios/js/buscador.js
@@ -185,6 +185,32 @@ function filtroEliminacion()
     }
 }
 
+function filtroCategoriaCandidata() 
+{
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("busquedaCandidata");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tablaCandidata");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) 
+    {
+    td = tr[i].getElementsByTagName("td")[5];
+        if (td) 
+        {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) 
+            {
+            tr[i].style.display = "";
+            } 
+            else 
+            {
+            tr[i].style.display = "none";
+            }
+        } 
+    }
+}
+
 function buscarC()
 {
     var aux = document.getElementById("busqueda").value;
